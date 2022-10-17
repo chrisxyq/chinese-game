ad7198bed04e770eb4fa7ea18e28cb40847e71d6
chrisxyq <yuanqixu@trip.com>
2022-10-17 17:56:32 +0800

    first commit

--- src/main/java/com/example/chinesegame/utils/ChineseParser.java
+++ src/main/java/com/example/chinesegame/utils/ChineseParser.java
@@ -1,9 +1,15 @@
 package com.example.chinesegame.utils;
 
+import com.example.chinesegame.dto.ParsedChinese;
+import org.springframework.util.CollectionUtils;
+
 import java.util.List;
 
-public class ChineseParser {
+public class ChineseParser implements IChineseParser{
     public static List<ParsedChinese> parse(List<String> list){
+        List<ParsedChinese> parsedChineseList =null;
+        if(CollectionUtils.isEmpty(list)){
 
+        }
     }
 }
---
+++ src/main/java/com/example/chinesegame/utils/IChineseParser.java
@@ -0,0 +1,11 @@
+package com.example.chinesegame.utils;
+
+import com.example.chinesegame.dto.ParsedChinese;
+
+import java.util.List;
+
+public interface IChineseParser {
+    static List<ParsedChinese> parse(List<String> list) {
+        return null;
+    }
+}
